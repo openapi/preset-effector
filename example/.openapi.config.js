95edf1534e1c1ad7dd59db51ad54ca4d47aaa501
Sergey Sova <mail@sergeysova.com>
2020-11-27 23:00:01 +0300

    feat: add effectorImport option

--- example/.openapi.config.js
+++ example/.openapi.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   file:
     'https://raw.githubusercontent.com/accesso-app/backend/master/api-public/openapi.yaml',
-  presets: [require.resolve('../')],
+  presets: [[require.resolve('../'), { effectorImport: 'effector-root' }]],
   outputDir: `${__dirname}/api`,
   mode: 'dev',
   templateFileNameCode: 'index.ts',
