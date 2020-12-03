b580d0ab6ec3dadb6d30d0d4fbb70887035f5c6a
Sergey Sova <mail@sergeysova.com>
2020-12-03 18:23:54 +0300

    feat: replace <br/> in comments to newline

--- src/comments.js
+++ src/comments.js
@@ -1,7 +1,11 @@
 const t = require('@babel/types');
 
 function addComment(node, text) {
-  const lines = text.split('\n').map((line) => line.trim());
+  const lines = text
+    .replace(/<br\s*\/>/gi, '\n')
+    .trim()
+    .split('\n')
+    .map((line) => line.trim());
   lines[0] = ` ${lines[0]}`;
   lines[lines.length - 1] = `${lines[lines.length - 1]} `;
   t.addComment(
